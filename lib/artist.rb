5e853592650be1f9952b7a89665953fe9a98a609
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-10-06 20:01:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,17 @@
 class Artist
   attr_accessor :name
+  all_artists = []
   
   def initialize(name)
     @name = name
   end
   
+  def all
+    all_artists
+  end
+  
+  def add_song
+    
   
   
   
