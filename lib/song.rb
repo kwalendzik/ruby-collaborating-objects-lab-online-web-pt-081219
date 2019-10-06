a1d38fec46d4e2092fe1db042a7e23e682dafeb1
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-10-06 20:27:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,14 @@ class Song
     @@all
   end
   
+  def self.new_by_filename(filename)
+    song = self.new
+    song.title = filename.split(" - ")[1]
+    song
+  end
   
-  
-  
+
+
   
   
 end
