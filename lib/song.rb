b9438fe624fd7d91360e368f5f103fe809d822d3
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-10-06 20:31:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist
   
-  @@all = []
+  @@all_songs = []
   
   def initialize(name)
     @name = name
@@ -9,11 +9,11 @@ class Song
   end
   
   def save
-   @@all << self
+   @@all_songs << self
   end
   
   def self.all
-    @@all
+    @@all_songs
   end
   
   def self.new_by_filename(filename)
