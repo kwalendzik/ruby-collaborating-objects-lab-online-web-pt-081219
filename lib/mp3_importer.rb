106fe2c42edfa16fb8c0fb41f5b1b3073251f177
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-10-06 20:55:08 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,17 @@
-class MP3Importer
- def import(list_of_filenames)
-   list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
- end
+class MP3Importer 
+    attr_accessor :path
+
+    def initialize(path)
+        @path = path
+    end 
+
+    def files 
+        Dir.entries(@path).select {|x| x.include?(".mp3")}
+    end 
+
+    def import 
+        self.files.each do |x| 
+            Song.new_by_filename(x)
+        end 
+    end 
 end
