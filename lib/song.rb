40141f1325cfb8eb0914fb67d4d0a51ced55849e
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-10-06 20:56:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,6 +20,12 @@ class Song
     song = self.new
     song.name = filename.split(" - ")
     song
+    
+    new_array = filename.split(" - ")
+        song = Song.new(new_array[1])
+        song.artist_name = new_array[0]
+        song
+      end
   end
 
    def artist_name=(name)
